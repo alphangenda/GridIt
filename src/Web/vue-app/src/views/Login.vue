0321aed6b81b9b319dd4a552e0065d114af2eb24
EdouardGE <creamsyko@gmail.com>
2026-03-01 13:25:42 -0500

    feat(logo) : Ajout du logo pour l'icon du site web, modification de l'appel de la route pour GridIt. Ajout du titre GridIt dans la page connexion et register (views)

--- src/Web/vue-app/src/views/Login.vue
+++ src/Web/vue-app/src/views/Login.vue
@@ -3,6 +3,11 @@
           class="form"
           :is-authentication="true"
           @keyup.enter="sendLoginRequest">
+
+        <h1 class="gridit-title">
+          <span class="grid">Grid</span><span class="it">It</span>
+        </h1>
+
         <Loader v-if="preventMultipleSubmit" />
         <FormInput :ref="addFormInputRef"
                    v-model="loginRequest.username"
@@ -111,3 +116,23 @@ async function sendLoginRequest() {
   preventMultipleSubmit.value = false;
 }
 </script>
+
+<style lang="scss" scoped>
+.gridit-title {
+    text-align: center;
+    margin-bottom: 1.5rem;
+    font-size: 2.4rem;
+    letter-spacing: -0.02em;
+}
+
+.gridit-title .grid {
+    font-weight: 400;
+    color: #000;
+}
+
+.gridit-title .it {
+    font-weight: 700;
+    color: #000;
+}
+</style>
+
