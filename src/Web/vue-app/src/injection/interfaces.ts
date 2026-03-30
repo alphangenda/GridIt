3d65bf457d7097319df2443e414043860bee313d
Alpha-Trésor Ngendanduwme <ngendandumwea6@gmail.com>
2026-03-29 21:55:05 -0400

    Correction affichage grilles (heberger) + Bonne redirection vers les groupes. Enlèvement d'ajout étudiant via cours.

--- src/Web/vue-app/src/injection/interfaces.ts
+++ src/Web/vue-app/src/injection/interfaces.ts
@@ -72,7 +72,7 @@ export interface IBookService {
 export interface IClassService {
   getAllClasses(): Promise<ClassItem[]>
 
-  createClass(name: string, students?: { number: string; firstName: string; lastName: string }[]): Promise<ClassItem>
+  createClass(name: string): Promise<ClassItem>
 
   deleteClass(classId: string): Promise<void>
 
